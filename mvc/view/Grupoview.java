ba5fed8efa717cf75193946e6e37f4eb3d777b89
shoegaze04 <53585235+shoegaze04@users.noreply.github.com>
2019-12-09 19:54:32 -0300

    Update Grupoview.java

--- mvc/view/Grupoview.java
+++ mvc/view/Grupoview.java
@@ -1,17 +1,17 @@
 package view;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import controller.Grupocontroller;
 import model.Contato;
 import model.Grupo;
 import observer.Observer;
 
+
 public class Grupoview implements Observer {
 
 	private Grupo model;
 	private Grupocontroller cont = new Grupocontroller();
+
 
 	@Override
 	public void update() {
@@ -20,9 +20,9 @@ public class Grupoview implements Observer {
 
 	public void display() {
 		System.out.println("---------------------------------------------\n");
-		System.out.println("Nome: " + model.getNome());
+		System.out.println("Nome do grupo: " + model.getNome());
 		
-		for(Contato c: cont.contatos) {
+		for(Contato c: cont.getModel().contatos) {
 			System.out.println("---------------------------------------------\n");
 			System.out.println("Nome: "+c.getNome());
 			System.out.println("Numero: "+c.getNumero());
@@ -45,6 +45,14 @@ public class Grupoview implements Observer {
 
 	public Grupocontroller getCont() {
 		return cont;
+	}
+
+	public Grupo getModel() {
+		return model;
+	}
+
+	public void setModel(Grupo model) {
+		this.model = model;
 	}
 	
 }
